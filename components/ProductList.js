873e8322ba0022b9a88d5d2e6851678f1768b5e9
Eduardo Valenzuela <valenzuela.eduardo@gmail.com>
2020-09-08 16:56:53 -0500

    Adding "Add" button and styling ProductList

--- components/ProductList.js
+++ components/ProductList.js
@@ -12,6 +12,11 @@ export default function ProductList() {
   const windowHeight = Dimensions.get('window').height;
   const productListHeight = windowHeight * 0.7;
 
+  const colors = [
+    '#eee',
+    '#FFF'
+  ]
+
   return (
     <View style={{ flex: 1, alignSelf: "stretch" }}>
       <Text style={[ commonStyle.h1 ]}>Product List</Text>
@@ -20,9 +25,9 @@ export default function ProductList() {
 
       <FlatList
         data={data}
-        renderItem={({ item }) => {
+        renderItem={({ item, index }) => {
           return (
-            <View style={[localStyles.rowContainer]}>
+            <View style={[localStyles.rowContainer, {backgroundColor: colors[index % colors.length]}]}>
               <Image
                 source={{ uri: item.photo }}
                 style={{ width: 50, height: 50 }}
@@ -41,13 +46,15 @@ export default function ProductList() {
         }}
         style={{ flexGrow: 0, height: productListHeight }}
       />
+      <TouchableHighlight style={[commonStyle.button, {width: 40, height: 40, justifyContent: 'center', alignItems: 'center', marginTop: 20, alignSelf:'flex-end', borderRadius: 20, backgroundColor: 'navy' }]}>
+        <Text style={{ color: '#FFF'}}> Add </Text>
+      </TouchableHighlight>
     </View>
   );
 }
 
 const localStyles = StyleSheet.create({
   rowContainer: {
-    backgroundColor: "yellow",
     flexDirection: "row",
     flex: 1,
     paddingHorizontal: 5,
